c656b5fbbaba4cd9f6b47a4e85194b9e0fdfec4a
Renata Mayra <renatamayra.mendes@gmail.com>
2025-10-04 13:13:18 -0300

    finalizando o projeto

--- script.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('.switch');
-const body = document.body;
-const text = document.querySelector('#switch-toggle p');
-
-toggle.addEventListener('click', () => {
-  body.classList.toggle('light');
-  text.textContent = body.classList.contains('light') ? 'Claro' : 'Escuro';
-});
